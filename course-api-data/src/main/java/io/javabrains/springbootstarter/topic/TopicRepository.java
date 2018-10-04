a1a1e9d3ab82fc8e2c89a0ab38ca31b4bfa7827a
Bernadett93 <szenti.berni@gmail.com>
2018-10-04 16:20:42 +0200

    Add files via upload

--- course-api-data/src/main/java/io/javabrains/springbootstarter/topic/TopicRepository.java
+++ course-api-data/src/main/java/io/javabrains/springbootstarter/topic/TopicRepository.java
@@ -1,11 +1,15 @@
 package io.javabrains.springbootstarter.topic;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 //methods with getAllTopics(), getTopic(String id), updateTopic(Topic t), deleteTopic(String id)
 //de a fentieket nem kell megcsinálni
-public interface TopicRepository extends CrudRepository<Topic, String> /*tartalmazza a logikát minden entityhez*/{
+public interface TopicRepository extends JpaRepository<Topic, String> /*tartalmazza a logikát minden entityhez*/{
     //CrudRepository: az entity maga: Topic először, aztán az Id típusa
 
+    //mégis JPArepository Roki mondta
+
+    Topic findById(Long id);
 
 }
